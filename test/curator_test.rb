7da92c2909f6ccd98c2182a10b043f123c71d6f8
Tyla Devon <tyladevon@gmail.com>
2019-09-17 10:42:32 -0600

    Add test for find_photograph_by_artist_id. Add multiple artists and photographs

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,6 +20,18 @@ class CuratorTest < Minitest::Test
                              artist_id: "2",
                              year: "1941"
     })
+    @photo_3 = Photograph.new({
+                             id: "3",
+                             name: "Identical Twins, Roselle, New Jersey",
+                             artist_id: "3",
+                             year: "1967"
+    })
+    @photo_4 = Photograph.new({
+                             id: "4",
+                             name: "Monolith, The Face of Half Dome",
+                             artist_id: "3",
+                             year: "1927"
+    })
     @artist_1 = Artist.new({
                           id: "1",
                           name: "Henri Cartier-Bresson",
@@ -34,6 +46,13 @@ class CuratorTest < Minitest::Test
                           born: "1902",
                           died: "1984",
                           country: "United States"
+    })
+    @artist_3 = Artist.new({
+                           id: "3",
+                           name: "Diane Arbus",
+                           born: "1923",
+                           died: "1971",
+                           country: "United States"
     })
   end
 
@@ -72,6 +91,18 @@ class CuratorTest < Minitest::Test
     @curator.add_photograph(@photo_2)
 
     assert_equal [@photo_2], @curator.find_photograph_by_id("2")
+  end
+
+  def test_can_find_photographs_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(@artist_3)
   end
 
 end
