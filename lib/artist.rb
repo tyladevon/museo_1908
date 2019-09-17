3f09fceb0aafd7d76a3c3088e0b3f9c539da04e9
Tyla Devon <tyladevon@gmail.com>
2019-09-17 09:30:54 -0600

    Add class Artist and initialize

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist
+  attr_reader :id, :name, :born, :died, :country
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+  end
+
+end
