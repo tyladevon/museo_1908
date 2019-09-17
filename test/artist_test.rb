262deb33e43a1eec5cb4a600e1c14ccc9b3b7603
Tyla Devon <tyladevon@gmail.com>
2019-09-17 09:30:34 -0600

    Add setup to Test and test initialize

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -6,8 +6,25 @@ require 'minitest/pride'
 
 class ArtistTest < Minitest::Test
 
-  def setup 
-
+  def setup
+    @artist = Artist.new({
+                        id: "2",
+                        name: "Ansel Adams",
+                        born: "1902",
+                        died: "1984",
+                        country: "United States"
+      })
   end
 
+  def test_it_exists
+    assert_instance_of Artist, @artist
+  end
+
+  def test_initialize
+    assert_equal "2" , @artist.id
+    assert_equal "Ansel Adams", @artist.name
+    assert_equal "1902", @artist.born
+    assert_equal "1984", @artist.died
+    assert_equal "United States", @artist.country
+  end
 end
