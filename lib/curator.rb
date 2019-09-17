f63192a875e7316a815b149ba9554ef6d91cb77d
Tyla Devon <tyladevon@gmail.com>
2019-09-17 09:48:44 -0600

    Add Curator class. Add initialize method and add_photograph method.

---
+++ lib/curator.rb
@@ -0,0 +1,13 @@
+class Curator
+  attr_reader :photographs, :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+
+  def add_photograph(photo)
+    @photographs << photo
+  end
+end
