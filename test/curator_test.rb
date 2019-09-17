c31f5a30b2d0ac88b444bfa81b14d63efd44e54b
Tyla Devon <tyladevon@gmail.com>
2019-09-17 10:06:43 -0600

    Refactor test and method name for can_add_photograph.

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -67,8 +67,11 @@ class CuratorTest < Minitest::Test
     assert_equal [@artist_1], @curator.find_artist_by_id("1")
   end
 
-  # def test_can_find_photograph_by_id
-  #   assert_equal [@photo_2], @curator.find_artist_by_id("2")
-  # end
+  def test_can_find_photograph_by_id
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_2], @curator.find_photograph_by_id("2")
+  end
 
 end
