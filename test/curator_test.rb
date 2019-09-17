69857e8af051689098a9e88b433d553a3623756d
Tyla Devon <tyladevon@gmail.com>
2019-09-17 11:43:12 -0600

    Add test for list_artists_with_multiple_photographs

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -105,4 +105,16 @@ class CuratorTest < Minitest::Test
     assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(@artist_3)
   end
 
+  def test_list_artists_with_multiple_photographs
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@artists_3], @curator.artists_with_multiple_photographs
+  end
+
 end
